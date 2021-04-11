34f76f699a4f2c0f465df327cb7357b6fa50087c
AnyaKovaleva <71429065+AnyaKovaleva@users.noreply.github.com>
2021-04-12 00:11:48 +0300

    d

--- 3d semester tasks/common/usefulFunctions.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef KOVALEVA_ANNA_B2_USEFULFUNCTIONS_HPP
-#define KOVALEVA_ANNA_B2_USEFULFUNCTIONS_HPP
-
-#include <iostream>
-
-std::istream& skipWsExceptNewLine(std::istream&);
-
-#endif //KOVALEVA_ANNA_B2_USEFULFUNCTIONS_HPP
